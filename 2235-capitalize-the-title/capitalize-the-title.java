c06bcdd7074a430791777c8bec88aae9ca7c90c4
Jeevaroshinibabu <jeevaroshinib.24cse@kongu.edu>
2026-04-02 11:32:23 +0530

    Time: 5 ms (44.08%) | Memory: 43.7 MB (75.27%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -1,7 +1,7 @@
 class Solution {
     public String capitalizeTitle(String title) {
         title=title.toLowerCase();
-        String[] n=title.split(" ");
+        String[] n=title.trim().split(" ");
         for(int i=0;i<n.length;i++){
             if(n[i].length()>2){
                    n[i] = n[i].substring(0, 1).toUpperCase() + n[i].substring(1);
