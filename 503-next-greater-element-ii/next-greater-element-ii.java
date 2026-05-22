d902f3c89808550d3193182b826051cc7486633a
Jeevaroshinibabu <jeevaroshinib.24cse@kongu.edu>
2026-05-22 09:47:53 +0530

    Time: 10 ms (76.45%) | Memory: 48.3 MB (75.28%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 2 * n; i++) {
+            int currentNum = nums[i % n];
+            while (!stack.isEmpty() && nums[stack.peek()] < currentNum) {
+                result[stack.pop()] = currentNum;
+            }
+            
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+        
+        return result;
+    }
+}
