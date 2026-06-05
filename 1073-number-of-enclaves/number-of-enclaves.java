e15eb6f36465ac1899f35bcc0347ae92e0a701e1
Jeevaroshinibabu <jeevaroshinib.24cse@kongu.edu>
2026-06-05 12:07:40 +0530

    Time: 6 ms (98.99%) | Memory: 61.9 MB (19.65%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -5,23 +5,23 @@ class Solution {
         int m=grid[0].length;
         for(int j=0;j<m;j++){
             if(grid[0][j]==1){
-                dfs(grid,0,j);
+                dfs(grid,0,j,n,m);
             }
             if(grid[n-1][j]==1){
-                dfs(grid,n-1,j);
+                dfs(grid,n-1,j,n,m);
             }
         }
 
         for(int i=0;i<n;i++){
             if(grid[i][0]==1){
-                dfs(grid,i,0);
+                dfs(grid,i,0,n,m);
             }
             if(grid[i][m-1]==1){
-                dfs(grid,i,m-1);
+                dfs(grid,i,m-1,n,m);
             }
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     c++;
                 }
@@ -29,14 +29,14 @@ class Solution {
         }
         return c;
     }
-    public void dfs(int[][] g,int r,int c){
-        if(r<0||c<0||r>=g.length||c>=g[0].length||g[r][c]==0){
+    public void dfs(int[][] g,int r,int c,int n,int m){
+        if(r<0||c<0||r>=n||c>=m||g[r][c]==0){
             return ;
         }
         g[r][c]=0;
-        dfs(g,r,c-1);
-        dfs(g,r,c+1);
-        dfs(g,r-1,c);
-        dfs(g,r+1,c);
+        dfs(g,r,c-1,n,m);
+        dfs(g,r,c+1,n,m);
+        dfs(g,r-1,c,n,m);
+        dfs(g,r+1,c,n,m);
     }
 }
