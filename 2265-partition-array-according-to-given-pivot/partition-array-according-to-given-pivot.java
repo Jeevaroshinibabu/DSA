e51323d4845aee0406b59b5d6debcc7529990003
Jeevaroshinibabu <jeevaroshinib.24cse@kongu.edu>
2026-06-08 18:50:36 +0530

    Time: 4 ms (99.24%) | Memory: 160 MB (49.83%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        Queue<Integer> a=new LinkedList<>();
-        Queue<Integer> b=new LinkedList<>();
-        Queue<Integer> c=new LinkedList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<pivot){
-                a.add(nums[i]);
-            }else if(nums[i]==pivot){
-                b.add(nums[i]);
-            }else{
-                c.add(nums[i]);
-            }
-        }
         int[] r=new int[nums.length];
         int p=0;
-        while(!a.isEmpty()){
-            r[p]=a.poll();
-            p++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<pivot){
+                r[p]=nums[i];
+                p++;
+            }
         }
-         while(!b.isEmpty()){
-            r[p]=b.poll();
-            p++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==pivot){
+                r[p]=nums[i];
+                p++;
+            }
         }
-         while(!c.isEmpty()){
-            r[p]=c.poll();
-            p++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>pivot){
+                r[p]=nums[i];
+                p++;
+            }
         }
         return r;
     }
