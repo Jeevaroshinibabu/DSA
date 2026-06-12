5059a305ea6699b2955a729cc56d99cc8ce4248e
Jeevaroshinibabu <jeevaroshinib.24cse@kongu.edu>
2026-06-12 10:19:32 +0530

    Time: 4 ms (63.12%) | Memory: 50.5 MB (21.60%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        for(int i=1;i<grid[0].length;i++){
+            grid[0][i]+=grid[0][i-1];
+        }
+
+        for(int i=1;i<grid.length;i++){
+            grid[i][0]+=grid[i-1][0];
+        }
+
+        for(int i=1;i<grid.length;i++){
+            for(int j=1;j<grid[0].length;j++){
+                grid[i][j]+=Math.min(grid[i][j-1],grid[i-1][j]);
+            }
+        }
+        int m=grid.length;
+        int n=grid[0].length;
+        return grid[m-1][n-1];
+    }
+}
