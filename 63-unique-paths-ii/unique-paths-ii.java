94bd46037bab0c5210185aafa5dc399225d28935
Jeevaroshinibabu <jeevaroshinib.24cse@kongu.edu>
2026-06-12 09:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (62.67%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] o) {
+        int m=o.length;
+        int n=o[0].length;
+
+        int[] dp=new int [n];
+        if(o[0][0]==0){
+            dp[0]=1;}
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(o[i][j]==1){
+                    dp[j]=0;
+                }
+                else if(j>0){
+                    dp[j]+=dp[j-1];
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
